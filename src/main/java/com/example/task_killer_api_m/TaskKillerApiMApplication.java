b77c6c403fdb9d327915860929a87033c2717215
a <a>
2021-11-26 10:44:20 +0100

    primer commit

---
+++ src/main/java/com/example/task_killer_api_m/TaskKillerApiMApplication.java
@@ -0,0 +1,13 @@
+package com.example.task_killer_api_m;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskKillerApiMApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskKillerApiMApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/task_killer_api_m/TaskKillerApiMApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.task_killer_api_m;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskKillerApiMApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
